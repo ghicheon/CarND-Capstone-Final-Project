7ced3a4391ceb5b257513d410284d06273bccd79
ghicheon <gebmania@gmail.com>
2019-08-17 08:27:33 +0900

    conditional integral

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -2,7 +2,6 @@
 MIN_NUM = float('-inf')
 MAX_NUM = float('inf')
 
-
 class PID(object):
     def __init__(self, kp, ki, kd, mn=MIN_NUM, mx=MAX_NUM):
         self.kp = kp
@@ -13,22 +12,44 @@ class PID(object):
 
         self.int_val = self.last_error = 0.
 
+        self.last_val = VAL_INIT
+
+        #conditional integral
+        self.integral_on = True
+
     def reset(self):
         self.int_val = 0.0
 
     def step(self, error, sample_time):
+        # integral on? or off?
+        #set False 
+        #if error is the same sign with PID output and it's saturated.
 
         integral = self.int_val + error * sample_time;
         derivative = (error - self.last_error) / sample_time;
 
-        val = self.kp * error + self.ki * integral + self.kd * derivative;
+        if self.integral_on == True:
+            val = self.kp * error + self.ki * integral + self.kd * derivative;
+        else:
+            val = self.kp * error + self.kd * derivative;
+            
+        self.last_val = val
 
+        saturated = True
         if val > self.max:
             val = self.max
         elif val < self.min:
             val = self.min
         else:
             self.int_val = integral
+            saturated = False
         self.last_error = error
+
+        same_sign = (error > 0 and val > 0 ) or (error < 0 and val < 0 )
+
+        if saturated and same_sign: 
+                self.integral_on = False
+        else:
+                self.integral_on = True  #turn on again!
 
         return val
